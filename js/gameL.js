39ada4c79a0da5e07c6dd7de830e350aa8f8f8c9
韦自奋 <542455925@qq.com>
2017-06-13 18:08:44 +0800

    游戏列表提交

---
+++ js/gameL.js
@@ -0,0 +1,79 @@
+/**
+ * Created by weizifen on 17/6/13.
+ */
+var gamelist={ xiuxian: [
+
+    { gameName: "挑战憋屁", url:"http://game.huoxingwan.com/Casual-games/?id=tzbp",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/tzbp.png" },
+
+    { gameName: "最强大脑", url:"http://game.huoxingwan.com/Casual-games/?id=zqdn",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/zqdn.png" },
+
+    { gameName: "挖鼻孔", url:"http://game.huoxingwan.com/Casual-games/?id=wbk",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/wbk.png" },
+
+    { gameName: "字牌翻翻翻", url:"http://game.huoxingwan.com/Casual-games/?id=zpfff" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/zpfff.png"},
+
+    { gameName: "坚持住，牛郎!", url:"http://game.huoxingwan.com/Casual-games/?id=jcznl" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/jcznl.png"},
+
+    { gameName: "命悬一线", url:"http://game.huoxingwan.com/Casual-games/?id=mxyx",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/mxyx.png" },
+
+    { gameName: "黑夜消灭怪物", url:"http://game.huoxingwan.com/Casual-games/?id=hyxmgw",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/hyxmgw.png" },
+
+    { gameName: "雪人往下掉", url:"http://game.huoxingwan.com/Casual-games/?id=xrwxd" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/xrwxd.jpg"},
+
+    { gameName: "抓住那逗比", url:"http://game.huoxingwan.com/Casual-games/?id=zzndb" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/zzndb.jpg"},
+
+    { gameName: "抓色狼", url:"http://game.huoxingwan.com/Casual-games/?id=zsl",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/zsl.jpg" },
+
+    { gameName: "全民追公交", url:"http://game.huoxingwan.com/Casual-games/?id=qmzgj" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/qmzgj.jpg"},
+
+    { gameName: "沿直线走加强版", url:"http://game.huoxingwan.com/Casual-games/?id=yzxzjqb",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/yzxzjqb.jpg" },
+
+    { gameName: "暴打渣男", url:"http://game.huoxingwan.com/Casual-games/?id=bdzn",imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/bdzn.jpg" },
+
+    { gameName: "旋转吧大宝剑", url:"http://game.huoxingwan.com/Casual-games/?id=dbj" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/dbj.png"},
+
+    { gameName: "废材战士", url:"http://game.huoxingwan.com/Casual-games/?id=fcys" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/fcys.png"},
+
+    { gameName: "忍者之刃", url:"http://game.huoxingwan.com/Casual-games/?id=rzzr" ,imageUrl:"http://game.huoxingwan.com/Casual-games/allGameImage/xiuxian/rzzr.png"}
+
+
+
+],
+
+    meinv: [
+
+    { gameName: "美女猜拳", url:"http://game.huoxingwan.com/Casual-games/?id=mncq" },
+
+    { gameName: "激情优衣库", url:"http://game.huoxingwan.com/Casual-games/?id=jqyyk" },
+
+    { gameName: "掀妹纸", url:"http://game.huoxingwan.com/Casual-games/?id=xmz" },
+
+    { gameName: "找出真妹子", url:"http://game.huoxingwan.com/Casual-games/?id=zczmz" },
+
+    { gameName: "解美女胸衣", url:"http://game.huoxingwan.com/Casual-games/?id=jmnxy" },
+
+    { gameName: "海滩偷吻", url:"http://game.huoxingwan.com/Casual-games/?id=httw" },
+
+    { gameName: "念湿恩,那些年我们一起撸过的老湿", url:"http://game.huoxingwan.com/Casual-games/?id=nse" }
+
+
+
+]
+
+// "musicians": [
+
+// { gameName: "挑战憋屁", "url":"McLaughlin" },
+
+// { gameName: "挑战憋屁", "url":"McLaughlin" },
+
+// { gameName: "挑战憋屁", "url":"McLaughlin" },
+
+// { gameName: "挑战憋屁", "url":"McLaughlin" },
+
+// { gameName: "挑战憋屁", "url":"McLaughlin" },
+
+// { gameName: "挑战憋屁", "url":"McLaughlin" },
+
+// { gameName: "挑战憋屁", "url":"McLaughlin" }
+
+// ]
+}
